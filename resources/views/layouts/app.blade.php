eecb185610e4ff1a20439009300bdbaf144e497c
deshanigtk <deshani.14@cse.mrt.ac.lk>
2017-04-07 22:41:18 +0530

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.dashboard')
+
+@section('head')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+@endsection
+
